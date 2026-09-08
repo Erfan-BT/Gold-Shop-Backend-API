f4c2a1666bd5efe1c6649d63ebadc19ebc7268a6
ERKL <92027793+Erfan-BT@users.noreply.github.com>
2026-09-09 00:52:38 +0330

    Handel Unique Constraint Error

--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -1,7 +1,8 @@
 import { Request, Response, NextFunction } from 'express'
-import { AppError, BadRequestError, InternalServerError, UnauthorizedError } from '../utils/appError.js' 
+import { AppError, BadRequestError, ConflictError, InternalServerError, UnauthorizedError } from '../utils/appError.js' 
 import { logger } from '../configs/pino.config.js'
 import multer from 'multer'
+import { UniqueConstraintError } from 'sequelize';
 // import { JsonWebTokenError, TokenExpiredError } from 'jsonwebtoken';
 
 export function errorHandler(
@@ -10,10 +11,16 @@ export function errorHandler(
     res : Response,
     _next : NextFunction
 ) {
-    if (!(err instanceof AppError)) {
-        req.logger.error({ error : String(err) }, 'Unhandled Error')
-        err = new InternalServerError(undefined, undefined, false)
+    if (err instanceof UniqueConstraintError) {
+        const errData = err.errors.map(item => ({
+            field: item.path,
+            value: item.value,
+            message: item.message
+        }))
+
+        err = new ConflictError('Unique Constraint Error', JSON.stringify(errData))
     }
+
     if (err instanceof multer.MulterError) {
         if (err.code === 'LIMIT_FILE_SIZE')
             err = new BadRequestError('Image File Is Too Large')
@@ -24,7 +31,14 @@ export function errorHandler(
     // if (err instanceof JsonWebTokenError || err instanceof TokenExpiredError) {
     //     err = new UnauthorizedError("Token Is Invalid")
     // }
-    req.logger.error({errorCode : err.statusCode, errorMsg : err.message, err}, "ERROR MIDDLEWARE")
+
+    if (!(err instanceof AppError)) {
+        req.logger.error({ error : String(err) }, 'Unhandled Error')
+        err = new InternalServerError(undefined, undefined, false)
+    }
+
+    req.logger.error({errorCode : err.statusCode, errorMsg : err.message, errorContext : err.context, err}, "ERROR MIDDLEWARE")
+
     res.status(err.statusCode).json({
         success : false,
         msg : err.message,
